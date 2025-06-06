3a243332b41ed0af34f3232a9bff5b15b8ddbec8
BennyEisner <benny.eisner@weather.com>
2025-06-06 14:03:52 -0400

    implemented create-db, seed-db

--- scripts/cli.py
+++ scripts/cli.py
@@ -5,8 +5,9 @@ from pathlib import Path
 import click
 import psycopg
 from dotenv import load_dotenv
+from psycopg.errors import OperationalError
 
-load_dotenv()  # Load environment variables from .env file if it existsj
+load_dotenv()  # Load environment variables from .env file if it exists
 
 # Default DB connection info (can be overridden by env vars)
 DB_CONFIG = {
@@ -16,6 +17,9 @@ DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "port": int(os.getenv("DB_PORT", "5432")),
 }
+
+PG_CONFIG = {**DB_CONFIG}
+PG_CONFIG["dbname"] = "postgres"
 
 
 def load_sql_and_execute(path: Path, conn: psycopg.Connection):
@@ -37,17 +41,41 @@ def cli():
 
 
 @cli.command("create-db")
-@click.argument(
-    "sql_file", type=click.Path(exists=True, dir_okay=False, path_type=Path)
-)
-def create_db(sql_file):
+def create_db():
     """
-    Create the DB schema using the given SQL_FILE.
+    Create a new database.
+    """
+    dbname = DB_CONFIG["dbname"]
+    try:
+        with psycopg.connect(**PG_CONFIG) as connection:
+            connection.autocommit = True
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT 1 FROM pg_database WHERE datname = %s", (dbname,)
+                )
+                if cursor.fetchone():
+                    click.echo(f"Database '{dbname}' exists already")
+                    return
+                cursor.execute(f'CREATE DATABASE "{dbname}"')
+                click.echo(f"Database '{dbname}' successfully created")
+    except OperationalError as e:
+        click.echo(f"Database creation failed: '{e}'")
 
-    Benny is soo cool.
+
+@cli.command("init-schema")
+@click.argument(
+    "schema_file", type=click.Path(exists=True, dir_okay=False, path_type=Path)
+)
+def init_schema(schema_file):
     """
-    with psycopg.connect(**DB_CONFIG) as conn:
-        load_sql_and_execute(sql_file, conn)
+    Initialize the database schema using the given SCHEMA_FILE.
+    """
+    try:
+        with psycopg.connect(**DB_CONFIG) as connection:
+            load_sql_and_execute(schema_file, connection)
+            click.echo("Database schema initialized")
+    except OperationalError as e:
+        click.echo(f"Schema init failed: {e}")
 
 
 @cli.command("seed-db")
@@ -56,8 +84,12 @@ def create_db(sql_file):
 )
 def seed_db(sql_file):
     """Seed the DB with example data using SQL_FILE."""
-    with psycopg.connect(**DB_CONFIG) as conn:
-        load_sql_and_execute(sql_file, conn)
+    try:
+        with psycopg.connect(**DB_CONFIG) as conn:
+            load_sql_and_execute(sql_file, conn)
+            click.echo("Database seeded successfully")
+    except OperationalError as e:
+        click.echo(f"Database seeding failed: {e}")
 
 
 @cli.command("dump-db")
