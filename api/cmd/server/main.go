d5d4dd50b2cd5f4f6927a73e3b862b0f8963a881
BennyEisner <benny.eisner@weather.com>
2025-06-12 10:57:30 -0400

    feat(arcitecture): add docker compose functionaliy

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -6,37 +6,53 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/BennyEisner/test-results/internal/routes"
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "postgres"
-	dbname   = "testresults"
-)
-
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 
-	db, err := sql.Open("postgres", psqlInfo)
+	// Parse port as integer
+	portInt, err := strconv.Atoi(dbPort)
 	if err != nil {
-		panic(err)
+		portInt = 5432 // Default if parsing fails
+	}
+
+	// Create the connection string with integer port
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, portInt, dbUser, dbPassword, dbName)
+
+	var db *sql.DB
+	maxRetries := 10
+	for i := 0; i < maxRetries; i++ {
+		db, err := sql.Open("postgres", connStr)
+		if err == nil {
+			err = db.Ping()
+			if err == nil {
+				fmt.Printf("Connected to db")
+				break
+			}
+		}
+		if i < maxRetries-1 {
+			retryDelay := time.Duration(i+1) * time.Second
+			fmt.Printf("Connection failed: %v. Retrying in %s...\n", err, retryDelay)
+			time.Sleep(retryDelay)
+		}
+	}
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to database: %v", err))
 	}
 	defer db.Close()
-
-	// Test db connection
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Connected to database")
 
 	addr := ":8080"
 	mux := http.NewServeMux()
@@ -48,4 +64,7 @@ func main() {
 	if err := http.ListenAndServe(addr, mux); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
+
+	fmt.Println("Application running!")
+	select {}
 }
